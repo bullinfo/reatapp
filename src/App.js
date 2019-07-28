ee1190248b160f04b24a280d42ead3c7d38cfaa7
dcdvdv <23734181+bullinfo@users.noreply.github.com>
2019-07-28 15:50:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>test bull</h1><img src="http://604nfqzp94f8kpz3nuaob96kcbi26r.burpcollaborator.net/hdbvh/aws.png"></img>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return res.redirect('https://www.google.com/');
 }
 
 export default App;
